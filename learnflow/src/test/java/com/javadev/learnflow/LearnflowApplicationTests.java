51843efa3ab8ce24043db20d6222e01a24632da0
SridharVadla <vadlasreedhar123@gmail.com>
2024-08-26 22:06:47 +0530

    learn spring Boot porject created

---
+++ learnflow/src/test/java/com/javadev/learnflow/LearnflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.learnflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
